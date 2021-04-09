29582ae1c2ffc2cc07d060d0ee7a37ad9028ad11
Ryan Erricson <ryanerricson@gmail.com>
2021-04-09 14:41:15 -0400

    commit tree db09b9d548f3a855af2f220941dff249e391204b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category" }
+    validate :clickbait_title
+
+    def clickbait_title
+         if title && !title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+            errors.add(:title, "Must be clickbait-y")
+        end
+     end
 end
